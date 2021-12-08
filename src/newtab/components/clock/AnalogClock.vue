6726ddaa1a334c2caf86a79e970fb73274ed21e2
cmd-cv <gaofengim@live.com>
2021-12-08 19:34:12 +0800

    fix: analog clock shake

--- src/newtab/components/clock/AnalogClock.vue
+++ src/newtab/components/clock/AnalogClock.vue
@@ -1,30 +1,15 @@
 <template>
   <div v-if="globalState.setting.clockAnalog.enabled" id="analog-clock">
     <div class="analog__container" :style="containerStyle">
-      <article
-        class="clock"
-        :style="`background-image: url(/assets/img/clock/${currTheme}/background.png);`"
-      >
-        <div
-          class="base marker"
-          :style="`background-image: url(/assets/img/clock/${currTheme}/marker.png);`"
-        ></div>
-        <div
-          class="base hour"
-          :style="`transform: rotateZ(${state.hourDeg}deg);`"
-        >
+      <article class="clock" :style="`background-image: url(/assets/img/clock/${currTheme}/background.png);`">
+        <div class="base marker" :style="`background-image: url(/assets/img/clock/${currTheme}/marker.png);`"></div>
+        <div class="base hour" :style="`transform: rotateZ(${state.hourDeg}deg);`">
           <img :src="`/assets/img/clock/${currTheme}/hour.png`" />
         </div>
-        <div
-          class="base minute"
-          :style="`transform: rotateZ(${state.minuteDeg}deg);`"
-        >
+        <div class="base minute" :style="`transform: rotateZ(${state.minuteDeg}deg);`">
           <img :src="`/assets/img/clock/${currTheme}/minute.png`" />
         </div>
-        <div
-          class="base second"
-          :style="`transform: rotateZ(${state.secondDeg}deg);`"
-        >
+        <div class="base second" :style="`transform: rotateZ(${state.secondDeg}deg);`">
           <img :src="`/assets/img/clock/${currTheme}/second.png`" />
         </div>
       </article>
@@ -34,12 +19,7 @@
 
 <script setup lang="ts">
 import dayjs from 'dayjs'
-import {
-  ANALOG_CLOCK_THEME,
-  globalState,
-  getLayoutStyle,
-  formatNumWithPixl,
-} from '@/logic'
+import { ANALOG_CLOCK_THEME, globalState, getLayoutStyle, formatNumWithPixl } from '@/logic'
 
 const CNAME = 'clockAnalog'
 
@@ -49,16 +29,28 @@ const state = reactive({
   secondDeg: 0,
 })
 
-const updateTime = () => {
+const initTime = () => {
   const h = dayjs().hour()
   const m = dayjs().minute()
   const s = dayjs().second()
   state.hourDeg = h * 30
-  state.minuteDeg = h * 21600 + m * 6
-  state.secondDeg = h * 21600 + m * 360 + s * 6
+  state.minuteDeg = m * 6
+  state.secondDeg = s * 6
 }
 
-updateTime()
+const updateTime = () => {
+  const s = dayjs().second()
+  const m = dayjs().minute()
+  state.secondDeg += 6
+  if (s === 0) {
+    state.minuteDeg += 6
+  }
+  if (m === 0) {
+    state.hourDeg += 30
+  }
+}
+
+initTime()
 
 const timer = setInterval(updateTime, 1000)
 
@@ -66,12 +58,7 @@ onUnmounted(() => {
   clearInterval(timer)
 })
 
-const currTheme = computed(
-  () =>
-    ANALOG_CLOCK_THEME.find(
-      item => item.value === globalState.setting.clockAnalog.theme,
-    )?.label || 'dark',
-)
+const currTheme = computed(() => ANALOG_CLOCK_THEME.find(item => item.value === globalState.setting.clockAnalog.theme)?.label || 'dark')
 
 const positionStyle = computed(() => getLayoutStyle(CNAME))
 
