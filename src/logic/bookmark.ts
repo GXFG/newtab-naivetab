62b5083e21e7ba7c2124a0b8b37cfeacb8949597
GXFG <gaofengim@live.com>
2022-02-08 19:51:50 +0800

    feat: keyboard add number & symbol

--- src/logic/bookmark.ts
+++ src/logic/bookmark.ts
@@ -2,73 +2,52 @@ import { useThrottleFn } from '@vueuse/core'
 import { useStorageLocal } from '@/composables/useStorageLocal'
 import { KEYBOARD_KEY_LIST, MERGE_SETTING_DELAY, localState, sleep, log, getDefaultBookmarkName } from '@/logic'
 
-const isInitialized = useStorageLocal('data-bookmark-initialized', false)
-
 export const localBookmarkList = useStorageLocal('data-bookmark', [] as BookmarkItem[])
 
-export const keyboardSettingRowList = computed(() => {
-  let splitIndexList: any = [[13, 23], [25, 34], [36, 43]]
+const keyboardSplitList = computed(() => {
+  let splitList: any = [[13, 23], [25, 34], [36, 43]]
   if (localState.setting.bookmark.isSymbolEnabled && localState.setting.bookmark.isNumberEnabled) {
-    splitIndexList = [[0, 13], [13, 25], [25, 36], [36]]
+    splitList = [[0, 13], [13, 25], [25, 36], [36]]
   } else if (localState.setting.bookmark.isSymbolEnabled) {
-    splitIndexList = [[13, 25], [25, 36], [36]]
+    splitList = [[13, 25], [25, 36], [36]]
   } else if (localState.setting.bookmark.isNumberEnabled) {
-    splitIndexList = [[[0, 10], [12, 13]], [13, 23], [25, 34], [36, 43]]
+    splitList = [[[0, 10], [12, 13]], [13, 23], [25, 34], [36, 43]]
   }
-  const rowList: any[] = []
-  for (const range of splitIndexList) {
-    if (range.length === 1) {
-      rowList.push(KEYBOARD_KEY_LIST.slice(range[0]))
-    } else {
-      if (Array.isArray(range[0])) {
-        // 处理特殊按键的拼接，如 [[0, 10], [12, 13]]
-        let tempList: any = []
-        for (const rangeItem of range) {
-          tempList = [...tempList, ...KEYBOARD_KEY_LIST.slice(rangeItem[0], rangeItem[1])]
-        }
-        rowList.push(tempList)
-      } else {
-        rowList.push(KEYBOARD_KEY_LIST.slice(range[0], range[1]))
-      }
-    }
-  }
-  return rowList
+  return splitList
 })
 
-export const keyboardRowList = computed(() => {
-  let splitIndexList: any = [[13, 23], [25, 34], [36, 43]]
-  if (localState.setting.bookmark.isSymbolEnabled && localState.setting.bookmark.isNumberEnabled) {
-    splitIndexList = [[0, 13], [13, 25], [25, 36], [36]]
-  } else if (localState.setting.bookmark.isSymbolEnabled) {
-    splitIndexList = [[13, 25], [25, 36], [36]]
-  } else if (localState.setting.bookmark.isNumberEnabled) {
-    splitIndexList = [[[0, 10], [12, 13]], [13, 23], [25, 34], [36, 43]]
-  }
+const getKeyboardList = (originList: any[]) => {
   const rowList: any[] = []
-  for (const range of splitIndexList) {
+  for (const range of keyboardSplitList.value) {
     if (range.length === 1) {
-      rowList.push(localBookmarkList.value.slice(range[0]))
+      rowList.push(originList.slice(range[0]))
     } else {
       if (Array.isArray(range[0])) {
-        // 处理特殊按键的拼接，如 [[0, 10], [12, 13]]
+        // 处理特殊按键的拼接，如：数字 + BS [[0, 10], [12, 13]]
         let tempList: any = []
         for (const rangeItem of range) {
-          tempList = [...tempList, ...localBookmarkList.value.slice(rangeItem[0], rangeItem[1])]
+          tempList = [...tempList, ...originList.slice(rangeItem[0], rangeItem[1])]
         }
         rowList.push(tempList)
       } else {
-        rowList.push(localBookmarkList.value.slice(range[0], range[1]))
+        rowList.push(originList.slice(range[0], range[1]))
       }
     }
   }
   return rowList
-})
+}
+
+export const keyboardSettingRowList = computed(() => getKeyboardList(KEYBOARD_KEY_LIST))
+
+export const keyboardRowList = computed(() => getKeyboardList(localBookmarkList.value))
+
+const isInitialized = useStorageLocal('data-bookmark-initialized', false)
 
 export const initBookmarkListData = () => {
-  log('initBookmarkListData')
   if (isInitialized.value) {
     return
   }
+  log('initBookmarkListData')
   localBookmarkList.value = []
   KEYBOARD_KEY_LIST.forEach((key: string) => {
     localBookmarkList.value.push({
@@ -83,12 +62,12 @@ export const initBookmarkListData = () => {
 const mergeBookmarkSetting = useThrottleFn(async() => {
   log('Merge BookmarkSetting')
   if (!isInitialized) {
-    await sleep(200)
+    await sleep(300)
   }
   for (const key of KEYBOARD_KEY_LIST) {
     const index = KEYBOARD_KEY_LIST.indexOf(key)
     const item = localState.setting.bookmark.keymap[key]
-    // 初始化无数据的按键
+    // 初始化无设置数据的按键
     if (!item) {
       localBookmarkList.value[index] = {
         key,
