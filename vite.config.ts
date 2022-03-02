398e4ce2947abcc7896147b5f876614f75277fe0
GXFG <gaofengim@live.com>
2022-03-02 20:34:47 +0800

    chore: use esbuild

--- vite.config.ts
+++ vite.config.ts
@@ -100,10 +100,10 @@ export default defineConfig(({ command }) => ({
     outDir: r('extension/dist'),
     emptyOutDir: false,
     sourcemap: isDev ? 'inline' : false,
-    // https://developer.chrome.com/docs/webstore/program_policies/#:~:text=Code%20Readability%20Requirements
-    terserOptions: {
-      mangle: false,
-    },
+    // minify: 'terser',
+    // terserOptions: {
+    //   mangle: false, // https://developer.chrome.com/docs/webstore/program_policies/#:~:text=Code%20Readability%20Requirements
+    // },
     rollupOptions: {
       input: {
         // background: r('src/background/index.html'),
