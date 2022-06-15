45a7d34a0f0d5b560570396bafc204cb7a66982b
GXFG <gaofengim@live.com>
2022-06-15 20:07:32 +0800

    test: ga4

--- src/logic/ga.ts
+++ src/logic/ga.ts
@@ -9,10 +9,10 @@ import { log } from './util'
  * @param opt_noninteraction 为true时，表示将不会在跳出率计算中使用事件命中
  */
 export const gaEvent = (category: string, action: string, opt_label: string, opt_value?: number, opt_noninteraction?: boolean) => {
-  if (import.meta.env.DEV) {
-    return
-  }
-  const param: any = ['_trackEvent', category, action, opt_label]
+  // if (import.meta.env.DEV) {
+  //   return
+  // }
+  const param: any = ['event', category, action, opt_label]
 
   if (opt_value !== undefined) {
     param.push(opt_value)
@@ -21,5 +21,5 @@ export const gaEvent = (category: string, action: string, opt_label: string, opt
     param.push(opt_noninteraction)
   }
   log('ga', param.slice(1).join(','))
-  window._gaq.push(param)
+  window.dataLayer.push(param)
 }
