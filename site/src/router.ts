e2fe96902e504db29a731da5bb5e008206ccf728
GXFG <gxfgim@outlook.com>
2026-07-15 20:37:48 +0800

    Update router history base URL to use environment variable

--- site/src/router.ts
+++ site/src/router.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './pages/home/index.vue'
 
 const router = createRouter({
-  history: createWebHistory('/newtab-naivetab/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
